2b888e6e7bb0be8d2fc44a05efcef79153799cc4
ash_weaver <ash@ashes.computer>
2024-06-15 13:59:41 -0700

    pull geographic data from eve esi

---
+++ geography_updater.py
@@ -0,0 +1,85 @@
+import requests
+
+# flags
+UPDATE_REGIONS = True
+UPDATE_CONSTELLATIONS = False
+UPDATE_SYSTEMS = False
+
+output_location = "csv/geography/"
+
+# requests data from EVE ESI for route
+# returns dictionary from json response
+# route example: "/regions/" or "/regions/10000003/"
+def request_data(route):
+    url_head = "https://esi.evetech.net/latest"
+    datasource = "?datasource=tranquility"
+    url = url_head + route + datasource
+    print("requesting " + route + "...")
+    return requests.get(url).json()
+
+print("hello captain :)")
+
+if UPDATE_REGIONS:
+    print("updating region information...")
+    # /universe/regions/
+        # [region_id]
+    # /universe/regions/id/
+        # region_id, name, ?description, [constellations]
+    response = request_data("/universe/regions/")
+    file_regions = open(output_location + "regions.csv", "w")
+    file_regions.write("region_id,name\n")
+    for id in response:
+        region = request_data("/universe/regions/" + str(id) + "/")
+        file_regions.write(
+            str(region["region_id"]) + "," +
+            "'" + region["name"] + "'"
+            + "\n")
+    file_regions.close()
+
+if UPDATE_CONSTELLATIONS:
+    print("updating constellation information...")
+    # /universe/constellations/ 
+        # [constellations_id]
+    # /universe/constellations/id/
+        # constellation_id, name, position, region_id, [systems]
+    constellations = request_data("/universe/constellations/")
+    file_constellations = open(output_location + "constellations.csv", "w")
+    file_constellations.write("constellation_id,name,region_id\n")
+    for id in constellations:
+        constellation = request_data("/universe/constellations/" + str(id) + "/")
+        file_constellations.write(
+            str(constellation["constellation_id"]) + "," +
+            "'" + constellation["name"] + "'" + "," +
+            str(constellation["region_id"]) + "," +
+            "\n")
+    file_constellations.close()
+
+if UPDATE_SYSTEMS:
+    print("updating system information...")
+    # /universe/systems/ 
+        # [system_id]
+    # /universe/systems/id/
+        # constellation_id
+        # name
+        # ?[planets]
+        # position
+        # ?security_class
+        # security_status
+        # ?star_id
+        # ?stargates
+        # ?stations
+        # system_id
+    systems = request_data("/universe/systems/")
+    file_systems = open(output_location + "systems.csv", "w")
+    file_systems.write("system_id,name,security_status,constellation_id\n")
+    for id in systems:
+        system = request_data("/universe/systems/" + str(id) + "/")
+        file_systems.write(
+            str(system["system_id"]) + "," +
+            "'" + system["name"] + "'" + "," +
+            "'" + str(system["security_status"]) + "'" + "," +
+            str(system["constellation_id"]) + "," +
+            "\n")
+    file_systems.close()
+
+print("all done captain :)")
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Eve Online General Store Helper (GSH)
+A project to identify regional markets ripe for a general store and then track store information.
+
+The project is broken up into the following:
+
+## Identify Markets
+Track region information such as ship activity (ship kills, pod kills, pirate kills) and local availability of consumable resources (munitions, containers, boosters, repair materials).
+
+### Regional Market Analysis
+Inspect systems and stations throughout a region to find concentrated market activity. In effect, find where are people currently buying, selling, and keeping their local base of operations.
+
+## Source Merchandise
+Cost analysis of buying finished product in large market hubs against buying raw resources and producing products myself.
+
+## Store Accounting
+Once a general store is set up and stocked, track the buy and sell orders over time to understand overall profitability and market stability.
+
+## Buying Loot
+Cost analysis of buying loot locally and selling it in major market hubs.
+
+# Implementation Details
+## EVE ESI
+Using python, the GSH will poll the [EVE ESI](https://esi.evetech.net/ui/) for data. Some data, like geographic information will need to be checked infrequently, while other information will be getting polled often, such as player activity statistics (hourly) or market data (daily).
+
+The data is returned as JSON objects that will be converted and stored as csv files to aid with postgresql integration.
+
+## Number Crunching
+The second part of the GSH is using a postgresql database to store and crunch the numbers.
+
+## Website Display
+TBD
