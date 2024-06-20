6193e526cef233cb55a38ad961d55b447cca8eb6
ash_weaver <ash@ashes.computer>
2024-06-19 17:31:16 -0700

    geography view

--- sql/geography.sql
+++ sql/geography.sql
@@ -19,3 +19,14 @@ create table systems (
      security_status numeric,
     constellation_id int
 );
+
+create or replace view geography as 
+select regions.id as region_id,
+regions.name as region_name,
+constellations.id as constellation_id,
+constellations.name as constellation_name,
+systems.id as system_id,
+systems.name as system_name
+from regions
+join constellations on regions.id = constellations.region_id 
+join systems on constellations.id = systems.constellation_id;
